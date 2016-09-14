d9cf1d75aa448eb45d712882c93ba6d71eb96f88
am411 <nilegend@gmail.com>
2016-09-14 14:30:48 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i = 3;
+            2 = i;
+        //Operators
+        //PEMDAS
+        //Exponent 2^2
+        // *
+        // /
+        // %
+        // x++
+        // x = x + 1
+        // x--.--x
+        // x+=2 x = x + 2
+        // x-=2, x/=2, x*=2
+
+        //Lesson 5
+        //Integer Truncation
+        int x = 3;
+        int y = 2;
+        int quotient = x/y;
+        //What do you get?
+        // quotient = 1  3/2 = 1.5  remove the .5
+
+        //Variables
+        //constant
+
+        final double taxRate = .06;
+
+
+
+
+
+    }
+}
